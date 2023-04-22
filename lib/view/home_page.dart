d7199d2024d910b84fb8cea1d3fba625d216a365
cmtan-2000 <cmtan0108@gmail.com>
2023-04-22 23:29:35 +0800

    Revert "added navigation"
    This reverts commit 11ac07be807f775c1c94a4faf49544fd7f131d6c.

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,12 +1,11 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, duplicate_ignore
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('This is homepage')),
+    // Todo: Yuxuan this is Homepage
+    return const Scaffold(
+      body: Center(child: Text('Home Page')),
     );
   }
 }
