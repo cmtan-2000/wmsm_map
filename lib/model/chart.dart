a80a7c1593bf147b7282cf4dabef73ea2edd0507
Eunice Lim <eunicelim1520@gmail.com>
2023-06-29 01:33:27 +0800

    done reporting

--- lib/model/chart.dart
+++ lib/model/chart.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ChartData {
-  ChartData([this.x, this.y, this.color]);
+  ChartData(this.x, this.y, [this.color]);
 
-  String? x;
-  dynamic y;
+  final String x;
+  final y;
   Color? color;
-  int? quantity;
-
-  void setQuantity(int quantity){
-    this.quantity = quantity;
-  }
 }
-
-
-//
